79813ba3ea6773fbfbe94ca71716e18a9bfdebaf
nisharampra <115043583+nisharampra@users.noreply.github.com>
2023-10-15 14:54:39 +0800

    Add files via upload

---
+++ js validation mini project/index.2.js
@@ -0,0 +1,18 @@
+
+// window.onload=function()
+
+// var mtBtn=document.getElementById("myBtn");
+// myBtn.addEvenListenner("mouseover",changeBtnText);
+// myBtn.addEvenListenner("click",changeBtnText);
+
+
+
+function changeBtnText()
+{
+    document.getElementById("myBtn").innerHTML="click it!";
+}
+
+function alertMsg(){
+    alert("hi here");
+}
+
